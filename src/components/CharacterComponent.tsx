b98956ddb0677d8dabc2e934b9d7c1f23acd8941
vich3er <cherednikviktoriya@gmail.com>
2025-01-06 15:48:14 +0200

    styles

--- src/components/CharacterComponent.tsx
+++ src/components/CharacterComponent.tsx
@@ -9,12 +9,15 @@ type CharacterProtorype = {
 
 const CharacterComponent:FC<CharacterProtorype> = ({character, children}) => {
     return (
-        <div className=''>
+        <div className='border  border-orange-400'>
             <h2 className='italic  text-orange-400 text-center text-2xl '>{character.name} {character.surname} </h2>
-            <p> age : {character.age} </p>
+
             <div className='flex row  items-center' >
                 <img className='inline  ' src={character.photo} alt=""/>
-                <p className='flex border border-orange-400 p-1  '>{children}</p>
+                <div>
+                    <p> age : {character.age} </p>
+                    <p className='flex border border-orange-400 p-1  '>{children}</p>
+                </div>
             </div>
 
         </div>
