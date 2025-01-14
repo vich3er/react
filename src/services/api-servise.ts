e70ee0359866d30c381fb0d673d65e09e42e88f5
vich3er <cherednikviktoriya@gmail.com>
2025-01-14 17:40:23 +0200

    ^^^

--- src/services/api-servise.ts
+++ src/services/api-servise.ts
@@ -4,3 +4,5 @@ import {ITodo} from "../models/ITodo.ts";
  export const getUsers = async ():Promise<ITodo[]> => {
      return await fetch(import.meta.env.VITE_API_URL).then(value => value.json())
  }
+
+//   "https://jsonplaceholder.typicode.com/todos"
