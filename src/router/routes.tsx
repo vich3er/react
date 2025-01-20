b86dde1c3ab75bff4b2ff96502aea200bab0128c
vich3er <cherednikviktoriya@gmail.com>
2025-01-20 18:35:25 +0200

    3_3 без урли

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -4,13 +4,10 @@ import CartsComp from "../components/CartsComp.tsx";
 
 export const routes = createBrowserRouter(
     [
-        {path: '', element: <MainPage/>
-            ,
-            children: [
-                {
-                    path: 'carts', element: <CartsComp/>
+        {path: '', element: <MainPage/>},
+        { path: 'carts', element: <CartsComp/>
                 }
-            ]
-        }
+
+
     ]
 )
